328c5550a5c569f663055b01469088055b5c9fa8
abrahan <abrahandev@gmail.com>
2024-01-10 10:43:05 +0100

    class pattern

---
+++ js/01-app.js
@@ -0,0 +1,12 @@
+// Class Pattern
+
+class persona {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+    }
+}
+
+const persona1 = new persona('Tomas', 'correo@pekky.com')
+
+console.log(persona1)
