86c1b112b4ff1f2837dc5d4a0d822325b8078eb7
abrahan <abrahandev@gmail.com>
2024-01-13 11:48:37 +0100

    Constructor Pattern exercice

--- js/02-app.js
+++ js/02-app.js
@@ -1,19 +1,32 @@
 // Constructor Pattern
 
 class Persona {
-    constructor(nombre, email) {
+    constructor(nombre, email, telefono) {
         this.nombre = nombre;
         this.email = email;
+        this.telefono = telefono;
     }
 }
 
 class Cliente extends Persona {
-    constructor(nombre, email, empresa) {
-        super(nombre, email)
-        this.empresa = empresa
+    constructor(nombre, email, empresa, telefono) {
+        super(nombre, email, telefono)
+        this.empresa = empresa;
     }
 }
 
-const cliente2 = new Cliente('Marcos', 'Correo@mail.com', 'Indra')
+const cliente2 = new Cliente('Marcos', 'Correo@mail.com', 'Indra', 12131244)
 
 console.log(cliente2)
+
+class Trabajador extends Cliente {
+    constructor(nombre, email, empresa, telefono, direccion) {
+        super(nombre, email, empresa, telefono);
+        this.direccion = direccion;
+    }
+
+}
+
+const trabajador = new Trabajador('Thomas', 'correo@correo', 'Indra', 123414124, 'Madrid')
+
+console.log(trabajador)
