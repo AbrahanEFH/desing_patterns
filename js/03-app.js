c55c41678a76e2beb734394bbb3268be434f5bb4
abrahan <abrahandev@gmail.com>
2024-01-16 08:39:18 +0100

    instancia en Singleton

--- js/03-app.js
+++ js/03-app.js
@@ -1,11 +1,21 @@
 // Singleton 
+
+let instancia = null;
+
+
 class Persona {
     constructor(nombre, email) {
-        this.nombre = nombre;
-        this.email = email;
+        if (!instancia) {
+            this.nombre = nombre;
+            this.email = email;
+            instancia = this;
+        } else {
+            return instancia;
+        }
     }
 }
 
 const persona = new Persona('Pepe', 'Correo@correo.com')
+const persona2 = new Persona('Llee', 'Correo@correo.com')
 
 console.log(persona)
