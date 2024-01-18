4e658e4935bf375cabb5c55efec57c763c21f61d
abrahan <abrahandev@gmail.com>
2024-01-18 09:48:12 +0100

    patron de diseño factory

--- js/04-app.js
+++ js/04-app.js
@@ -14,10 +14,15 @@ class InputHTML {
 class HTMLFactory {
     crearElemento(tipo, nombre) {
         switch (tipo) {
-
+            case 'text':
+                return new InputHTML('text', nombre)
 
             default:
                 return;
         }
     }
 }
+
+const elemento = new HTMLFactory();
+const inputText = elemento.crearElemento('text', 'nombre-cliente');
+console.log(inputText.crearInput());
