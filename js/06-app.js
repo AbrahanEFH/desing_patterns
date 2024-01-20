ac2c854f6b597b28ff1e4d099840a771b6aa1af3
abrahan <abrahandev@gmail.com>
2024-01-20 17:27:32 +0100

    tlf agregado

--- js/06-app.js
+++ js/06-app.js
@@ -1,8 +1,13 @@
 // Mixin Pattern
 
 class Persona {
-    constructor(nombre, email) {
+    constructor(nombre, email, tlf) {
         this.nombre = nombre;
         this.email = email;
+        this.tlf = tlf;
     }
 }
+
+const persona1 = new Persona(`nombre = ${this.nombre}`)
+
+console.log(persona1)
