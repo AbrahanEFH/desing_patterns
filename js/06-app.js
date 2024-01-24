04fa4b8e47c25dd9987c50422c03dec1ad7bc16d
abrahan <abrahandev@gmail.com>
2024-01-24 09:25:49 +0100

    con una clase mas

--- js/06-app.js
+++ js/06-app.js
@@ -8,6 +8,14 @@ class Persona {
     }
 }
 
+class Cliente {
+    constructor(nombre, email) {
+        this.nombre = nombre;
+        this.email = email;
+
+    }
+}
+
 const funcionesPersona = {
     mostrarInformacion() {
         console.log(`Nombre Persona: ${this.nombre}, Email: ${this.email} Tlf: ${this.tlf}`)
@@ -19,9 +27,13 @@ const funcionesPersona = {
 
 // Añadir funcionesPersona a la clase de persona
 Object.assign(Persona.prototype, funcionesPersona)
+Object.assign(Cliente.prototype, funcionesPersona)
 
 const cliente = new Persona('Thomas', 'correo@correo.com', 123124124)
 
 console.log(cliente)
 cliente.mostrarInformacion()
 cliente.mostrarNombre()
+
+const cliente2 = new Cliente('cliente', 'correo')
+console.log(cliente2)
