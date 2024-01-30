e0665845df58f11df67151b43a3ba503f1fa7a4e
abrahan <abrahandev@gmail.com>
2024-01-30 08:54:45 +0100

    Funciones y objetos para Mediador

--- js/08-app.js
+++ js/08-app.js
@@ -1,1 +1,18 @@
 // Patron de diseno Mediador
+
+function Vendedor(nombre) {
+
+}
+
+function Comprador(nombre) {
+
+}
+
+function Subasta() {
+
+}
+
+
+// Creacion de objetos
+
+const thomas = new Comprador('Thomas')
