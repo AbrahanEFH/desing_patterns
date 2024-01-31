d7a716ab39da32a30bb32ca4b3983428765e7040
abrahan <abrahandev@gmail.com>
2024-01-31 09:17:19 +0100

    Objetos con prototype

--- js/08-app.js
+++ js/08-app.js
@@ -1,7 +1,17 @@
 // Patron de diseno Mediador
 
 function Vendedor(nombre) {
+    this.nombre = nombre;
+    this.sala = null;
+}
 
+Vendedor.prototype = {
+    oferta: (articulo, precio) => {
+        console.log(`Tenemos el siguiente articulo ${articulo}, iniciamos con un precio de ${precio}`)
+    },
+    vendido: comprador => {
+        console.log(`vendido a ${comprador}`)
+    }
 }
 
 function Comprador(nombre) {
@@ -19,3 +29,6 @@ const thomas = new Comprador('Thomas')
 const pablo = new Comprador('Pablo')
 const vendedor = new Vendedor('vendedor de autos')
 const subasta = new Subasta('subasta')
+
+vendedor.oferta('Mustang 66', 33)
+vendedor.vendido('Pablo')
